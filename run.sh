21d7d9e1c272f5d1698d906a95eb074ff7278361
Prashant Agrawal <prashant@cse.iitd.ac.in>
2024-12-08 12:09:02 +0530

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run -p 5000:5000 -p 3000:3000 -p 7000:7000 -v $(pwd)/e-voting:/e-voting -it b3932b6840d0 /bin/bash
+sudo docker run -p 5000:5000 -p 3000:3000 -p 7000:7000 -v $(pwd)/e-voting:/e-voting -it evoting /bin/bash -c "cd evoting_localstorage/project_evoting; /root/.nvm/versions/node/v22.3.0/bin/node index"
