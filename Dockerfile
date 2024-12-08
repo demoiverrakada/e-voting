bd5afd6668b1d187a99dfd7fd8c74c2a36d9f883
Prashant Agrawal <prashant@cse.iitd.ac.in>
2024-12-08 18:13:24 +0530

    fix node_modules error

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,7 @@ FROM docker.io/demoiverrakada/evoting:updated1.0.0
 
 # Set working directory
 COPY src /app
-WORKDIR /app
+WORKDIR /app/
+
+# Install node modules
+RUN /bin/bash --login -c "cd evoting_localstorage/project_evoting && npm install"
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run -p 5000:5000 -p 3000:3000 -p 7000:7000 -v $(pwd)/e-voting:/e-voting -it evoting /bin/bash -c "cd evoting_localstorage/project_evoting; /root/.nvm/versions/node/v22.3.0/bin/node index"
+sudo docker run -p 5000:5000 -p 3000:3000 -p 7000:7000 -v $(pwd)/e-voting:/e-voting -it evoting /bin/bash -c "node index"
