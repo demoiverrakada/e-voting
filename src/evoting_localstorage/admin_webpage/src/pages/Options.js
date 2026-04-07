35e48f2644ada4d0876e21c13b2e70931cbaffc4
rdt3612 <raagineedturki@gmail.com>
2026-04-07 18:00:50 +0000

    fixed the endpoints

--- src/evoting_localstorage/admin_webpage/src/pages/Options.js
+++ src/evoting_localstorage/admin_webpage/src/pages/Options.js
@@ -26,7 +26,7 @@ function OptionsPage() {
     try {
       const token = sessionStorage.getItem('access_token');
       await axios.post(
-        'https://5000-01kk9e5t37w5v48yspx6hdpj3s.cloudspaces.litng.ai//reset-election',
+        'https://5000-01kk9e5t37w5v48yspx6hdpj3s.cloudspaces.litng.ai/reset-election',
         {},
         { headers: { authorization: `Bearer ${token}` } }
       );
--- src/evoting_localstorage/admin_webpage/src/pages/UploadBMDKeys.js
+++ src/evoting_localstorage/admin_webpage/src/pages/UploadBMDKeys.js
@@ -39,7 +39,7 @@ function UploadCandidate() {
         console.log(jsonData)
         const token = sessionStorage.getItem('access_token');
 
-        const response = await axios.post('https://5000-01kk9e5t37w5v48yspx6hdpj3s.cloudspaces.litng.ai//upload_bmd_keys', jsonData, {
+        const response = await axios.post('https://5000-01kk9e5t37w5v48yspx6hdpj3s.cloudspaces.litng.ai/upload_bmd_keys', jsonData, {
           headers: {
             authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
